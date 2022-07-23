3cfe054998da39c746b31fff710b8585fd591a07
iamirinad <irka_dimcheva@abv.bg>
2022-07-23 12:42:40 +0300

    using portals for side-drawer

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,7 +1,11 @@
+import ReactDOM from 'react-dom';
+
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-  return <aside className='side-drawer'>{props.children}</aside>
+  const content = <aside className='side-drawer'>{props.children}</aside>;
+
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 };
 
 export default SideDrawer;
