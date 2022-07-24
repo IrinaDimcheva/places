479511f75afb9603eb61aacf881a0e2c323647b2
iamirinad <irka_dimcheva@abv.bg>
2022-07-24 16:55:22 +0300

    rendering a map with google maps

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
