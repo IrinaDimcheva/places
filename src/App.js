6b84c277395eed1f184ef58f7b7f11c2944ea818
iamirinad <irka_dimcheva@abv.bg>
2022-07-25 20:23:01 +0300

    adding an auth-page and login-form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
+import Auth from './user/pages/Auth';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/:userId/places' element={<UserPlaces />} />
           <Route path='/places/new' element={<NewPlace />} />
           <Route path='/places/:placeId' element={<UpdatePlace />} />
+          <Route path='/auth' element={<Auth />} />
           <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </main>
---
+++ src/user/pages/Auth.js
@@ -0,0 +1,60 @@
+import Button from "../../shared/components/FormElements/Button";
+import Input from "../../shared/components/FormElements/Input";
+import Card from "../../shared/components/UIElements/Card";
+import { useForm } from "../../shared/hooks/form-hook";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+import './Auth.css';
+
+const Auth = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      email: {
+        value: '',
+        isValid: false
+      },
+      password: {
+        value: '',
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const authSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        <Input
+          id='email'
+          element='input'
+          type='email'
+          label='E-Mail'
+          validators={[VALIDATOR_EMAIL()]}
+          errorText='Please enter a valid email address.'
+          onInput={inputHandler}
+        />
+        <Input
+          id='password'
+          element='input'
+          type='password'
+          label='Password'
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText='Please enter a valid password, at least 5 characters.'
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>LOGIN</Button>
+      </form>
+    </Card>
+  );
+}
+
+export default Auth;
