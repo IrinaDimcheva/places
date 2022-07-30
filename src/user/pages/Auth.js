92180727f75124b0365ec1f79431b8fc879cb20f
iamirinad <irka_dimcheva@abv.bg>
2022-07-30 16:40:41 +0300

    sending a login request

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -56,11 +56,33 @@ const Auth = () => {
 
   const authSubmitHandler = async event => {
     event.preventDefault();
-    if (isLoginMode) {
+    setIsLoading(true);
 
+    if (isLoginMode) {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong, please try again.');
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch('http://localhost:5000/api/users/signup', {
           method: 'POST',
           headers: {
@@ -77,11 +99,9 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message);
         }
-        console.log(responseData);
         setIsLoading(false);
         auth.login();
       } catch (err) {
-        console.log(err);
         setIsLoading(false);
         setError(err.message || 'Something went wrong, please try again.');
       }
