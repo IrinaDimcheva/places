9104d484a9a3f528e542156f1dde32903edd502b
iamirinad <irka_dimcheva@abv.bg>
2022-08-02 17:42:15 +0300

    storing the token in the local-storage

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ function App() {
   const login = useCallback((uid, token) => {
     setToken(token);
     setUserId(uid);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({ userId: uid, token: token })
+    );
   }, []);
 
   const logout = useCallback(() => {
